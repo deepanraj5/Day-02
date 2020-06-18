c0a1aec65011550455ba31d1236ce8c4590bc64c
Deepan <Deepan@LAPTOP-NUHDER5L>
2020-06-18 13:30:09 +0530

    Programs

---
+++ programs/src/programs/ex2.java
@@ -0,0 +1,16 @@
+package programs;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=10;
+		int n2=5;
+		if(n1>n2)
+			System.out.println(n1);
+		else
+			System.out.println(n2);
+
+	}
+
+}
---
+++ programs/src/programs/ex4.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		int n2=5;
+		int sum=n+n2;
+		if(sum%2==0)
+			System.out.println("EVEN");
+		else
+			System.out.println("ODD");
+
+	}
+
+}
